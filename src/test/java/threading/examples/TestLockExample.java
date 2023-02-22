d9613b676f62a1cf9ad5c4e9fccffd2f43ca675e
Mark H <markhawkins@blueyonder.co.uk>
2023-02-22 20:58:48 +0000

    Added base unit test.

---
+++ src/test/java/threading/examples/TestLockExample.java
@@ -0,0 +1,14 @@
+package threading.examples;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestLockExample {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
